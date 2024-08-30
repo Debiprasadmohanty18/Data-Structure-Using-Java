492270df3841cb8c191464d2090bbb86508aaf81
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-30 18:24:31 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		int[] nums = {5,-7,9,2,-4,0};
+		System.out.println(Arrays.toString(insertionSort(nums)));
+	}
+	
+	static int[] insertionSort(int[] nums)
+	{
+		for(int i=0;i<nums.length-1;i++)
+		{
+			for(int j=i+1;j>0;j--)
+			{
+				if(nums[j] < nums[j-1])
+				{
+					int temp = nums[j-1];
+					nums[j-1] = nums[j];
+					nums[j] = temp;
+				}
+				else 
+				{
+					break;
+				}
+			}
+		}
+		return nums;
+	}
+}
